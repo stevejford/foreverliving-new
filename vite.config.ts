9f9bb7155ff25832cd82acc570e1d41eee7a883f
stevejford1 <157422783+stevejford1@users.noreply.github.com>
2024-10-07 04:20:08 +1100

    Prepare for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import autoprefixer from 'autoprefixer'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/foreverliving-new/', // Add this line
   build: {
     minify: 'terser',
     terserOptions: {
